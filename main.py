95b1bc44093124c428d9043892f0228ac37a1a57
shivanibhalsakle <100023956+shivanibhalsakle@users.noreply.github.com>
2022-03-05 17:25:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,58 +2,67 @@ import csv
 import sys
 
 
+
 while True:
-    #assuming file format : lastname,firstname,phonenumber,mail
-    with open( "assets/rakshins.csv", 'r' ) as source:
-        reader = csv.reader( source )
-         
+     print("Would you like to:\n1. Generate Vcards\n2. End Program")
+     main_opt = int(input("Your Choice: "))
 
-        i = 0
-        print("Do you want to write:\n 1. An individual Vcard \n 2. All Vcards in the file  ")
-        type = int(input("Your choice:"))
+     if main_opt == 1:
+         #assuming file format : lastname,firstname,phonenumber,mail
+         with open( "assets/rakshins.csv", 'r' ) as source:
 
-        if type==1:
-
-            for row in reader:
-                #write in individual vcf
-                vcf = open(row[1] + ' ' + row[0] + ".vcf", 'w')
-                vcf.write( 'BEGIN:VCARD' + "\n")
-                vcf.write( 'VERSION:2.1' + "\n")
-                vcf.write( 'N:' + row[0] + ';' + row[1] + "\n")
-                vcf.write( 'FN:' + row[1] + ' ' + row[0] + "\n")
-                vcf.write( 'ORG:' + 'ATI' + "\n")
-                vcf.write( 'TEL;CELL:' + row[2] + "\n")
-                vcf.write( 'EMAIL:' + row[3] + "\n")
-                vcf.write( 'END:VCARD' + "\n")
-                vcf.write( "\n")
-                vcf.close()
+                reader = csv.reader( source )
             
-               
 
-        
+                i = 0
+                print("Do you want to write:\n 1. An individual Vcard \n 2. All Vcards in the file  ")
+                type = int(input("Your choice:"))
 
-        if type==2:
+                if type==1:
+                    for row in reader:
+                        #write in individual vcf
+                        vcf = open(row[1] + ' ' + row[0] + ".vcf", 'w')
+                        vcf.write( 'BEGIN:VCARD' + "\n")
+                        vcf.write( 'VERSION:2.1' + "\n")
+                        vcf.write( 'N:' + row[0] + ';' + row[1] + "\n")
+                        vcf.write( 'FN:' + row[1] + ' ' + row[0] + "\n")
+                        vcf.write( 'ORG:' + 'ATI' + "\n")
+                        vcf.write( 'TEL;CELL:' + row[2] + "\n")
+                        vcf.write( 'EMAIL:' + row[3] + "\n")
+                        vcf.write( 'END:VCARD' + "\n")
+                        vcf.write( "\n")
+                        vcf.close()
+                
+                
 
-            for row in reader:
-             #write in the "ALL.vcf" file.
-                allvcf = open('ALL.vcf', 'w')
-                allvcf.write( 'BEGIN:VCARD' + "\n")
-                allvcf.write( 'VERSION:2.1' + "\n")
-                allvcf.write( 'N:' + row[0] + ';' + row[1] + "\n")
-                allvcf.write( 'FN:' + "Bootcamp " + row[1] + ' ' + row[0] + "\n")
-                allvcf.write( 'ORG:' + 'Bootcamp' + "\n")
-                allvcf.write( 'TEL;CELL:' + row[2] + "\n")
-                allvcf.write( 'EMAIL:' + row[3] + "\n")
-                allvcf.write( 'END:VCARD' + "\n")
-                allvcf.write( "\n")
+            
 
-           
+                if type==2:
 
-        i += 1
+                    for row in reader:
+                    #write in the "ALL.vcf" file.
+                        allvcf = open('ALL.vcf', 'w')
+                        allvcf.write( 'BEGIN:VCARD' + "\n")
+                        allvcf.write( 'VERSION:2.1' + "\n")
+                        allvcf.write( 'N:' + row[0] + ';' + row[1] + "\n")
+                        allvcf.write( 'FN:' + "Bootcamp " + row[1] + ' ' + row[0] + "\n")
+                        allvcf.write( 'ORG:' + 'Bootcamp' + "\n")
+                        allvcf.write( 'TEL;CELL:' + row[2] + "\n")
+                        allvcf.write( 'EMAIL:' + row[3] + "\n")
+                        allvcf.write( 'END:VCARD' + "\n")
+                        allvcf.write( "\n")
 
-        allvcf.close()
-        print (str(i) + " vcf cards generated.")
+                
 
+                i += 1
+
+                allvcf.close()
+                print (str(i) + " vcf cards generated.")
+
+
+     if main_opt == 2: 
+         print('Okay, see you soon!')
+         break          
 
 def main(args):
     if len(args) != 2:
